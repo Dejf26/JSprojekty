0e7b7a1471bb893b0c1a6dda77201248f3a3c3b2
Dawid Migdał <dejf026@gmail.com>
2023-10-09 13:46:55 +0200

    Normal done

--- Lab 1 - rozgrzewka/main.js
+++ Lab 1 - rozgrzewka/main.js
@@ -1,24 +1,67 @@
 // notatnik z zajęć
-const liczba1 = document.querySelector('#liczba1')
-const liczba2 = document.querySelector('#liczba2')
-const liczba3 = document.querySelector('#liczba3')
-const liczba4 = document.querySelector('#liczba4')
 
-const btnPrzelicz = document.querySelector('#przelicz')
+//
+const value1 = document.querySelector('#value1')
+const value2 = document.querySelector('#value2')
+const value3 = document.querySelector('#value3')
+const value4 = document.querySelector('#value4')
 
-const sumaWyn = document.querySelector('#suma')
-const sredniaWyn = document.querySelector('#srednia')
-const minWyn = document.querySelector('#min')
-const maxWyn = document.querySelector('#max')
+const btnCalculate = document.querySelector('#calculate')
+
+const sumCalc = document.querySelector('#sum')
+const avgCalc = document.querySelector('#avg')
+const minCalc = document.querySelector('#min')
+const maxCalc = document.querySelector('#max')
 
 
-btnPrzelicz.addEventListener('click', () => {
-    sumaWyn.innerHTML = 'Suma: '+ (parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value))
-    sredniaWyn.innerHTML = 'Średnia: '+ (parseInt(liczba1.value) + parseInt(liczba2.value) + parseInt(liczba3.value) + parseInt(liczba4.value)) /4
-    minWyn.innerHTML = 'Min: '+ Math.min(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value), parseInt(liczba4.value))
-    maxWyn.innerHTML = 'Max: '+ Math.max(parseInt(liczba1.value), parseInt(liczba2.value), parseInt(liczba3.value), parseInt(liczba4.value))
-
-    
-    // console.log(liczba1.value)
-     
+btnCalculate.addEventListener('click', () => {
+    sumCalc.innerHTML = 'Suma: '+ (parseInt(value1.value) + parseInt(value2.value) + parseInt(value3.value) + parseInt(value4.value))
+    avgCalc.innerHTML = 'Średnia: '+ (parseInt(value1.value) + parseInt(value2.value) + parseInt(value3.value) + parseInt(value4.value)) /4
+    minCalc.innerHTML = 'Min: '+ Math.min(parseInt(value1.value), parseInt(value2.value), parseInt(value3.value), parseInt(value4.value))
+    maxCalc.innerHTML = 'Max: '+ Math.max(parseInt(value1.value), parseInt(value2.value), parseInt(value3.value), parseInt(value4.value))
 })
+
+//
+const result2 = document.querySelector('#result2')
+const num=document.getElementsByClassName('num')
+
+let div, input, button;
+
+function Add(){
+    div = document.createElement("div")
+    input = document.createElement('input')
+    button = document.createElement('button')
+    button.innerHTML="Usuń"
+    button.addEventListener('click',(ev)=>{
+        ev.currentTarget.parentNode.remove()
+        Calculate()
+    });
+
+    input.type = "number"
+    input.value = 0
+    input.className = "num"
+
+    input.addEventListener('input', Calculate)
+    div.appendChild(input)
+    div.appendChild(button)
+
+    document.querySelector("#fields").appendChild(div)
+    Calculate()
+}
+
+function Calculate(){
+    let arr = []
+    for (let i of num) {
+        arr.push(parseInt(i.value))
+    }
+
+    const res = arr.reduce((sum, val) => sum + val)
+    result2.innerHTML = `Suma: ${res}, Średnia: ${res/arr.length}, 
+    Min: ${Math.min(...arr)}, Max: ${Math.max(...arr)}`
+}
+
+let add = document.getElementById("add")
+add.addEventListener('click', Add)
+Add()
+Add()
+Add()
